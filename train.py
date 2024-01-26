f156e2dad98925dff0654113f621eec1addc0b01
Prennk <email.affanardana@gmail.com>
2024-01-26 10:50:13 +0700

    [update] hyperparams

--- train.py
+++ train.py
@@ -481,7 +481,10 @@ if __name__ == "__main__":
         #-------------------------------------------------------------------#
         #   判断当前batch_size，自适应调整学习率
         #-------------------------------------------------------------------#
-        nbs             = 64
+        # nbs             = 64
+        nbs             = Unfreeze_batch_size
+        # lr_limit_max    = 1e-3 if optimizer_type in ['adam', 'adamw'] else 5e-2
+        # lr_limit_min    = 3e-4 if optimizer_type in ['adam', 'adamw'] else 5e-4
         lr_limit_max    = 1e-3 if optimizer_type in ['adam', 'adamw'] else 5e-2
         lr_limit_min    = 3e-4 if optimizer_type in ['adam', 'adamw'] else 5e-4
         Init_lr_fit     = min(max(batch_size / nbs * Init_lr, lr_limit_min), lr_limit_max)
