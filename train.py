a4c6338f242ec5e2439b46d2813e34e7aa7def64
Prennk <email.affanardana@gmail.com>
2024-01-27 12:48:34 +0700

    [add] road sign detection

--- train.py
+++ train.py
@@ -225,8 +225,8 @@ if __name__ == "__main__":
     #                           Adam可以使用相对较小的UnFreeze_Epoch
     #   Unfreeze_batch_size     模型在解冻后的batch_size
     #------------------------------------------------------------------#
-    UnFreeze_Epoch      = 300
-    Unfreeze_batch_size = 64
+    UnFreeze_Epoch      = 600
+    Unfreeze_batch_size = 8
     #------------------------------------------------------------------#
     #   Freeze_Train    是否进行冻结训练
     #                   默认先冻结主干训练后解冻训练。
